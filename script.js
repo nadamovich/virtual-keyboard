12c59c899349285400c2726c7280426818f99fa3
nadamovich <nikita.adamovich@gmail.com>
2022-05-10 02:16:56 +0300

    feat: page markup function,  enter characters by mouse function,  animation function

---
+++ script.js
@@ -0,0 +1,533 @@
+let keysArr = [[ 
+    {
+    "ENG": "`",
+    "RUS": "ё",
+    "EngShift": "¬",
+    "RusShift": "Ё",
+    "code": "Backquote"
+   },
+     {
+      "ENG": "1",
+      "RUS": "1",
+      "EngShift": "!",
+      "RusShift": "!",  
+      "code": "Digit1"
+   },
+     {
+      "ENG": "2",
+      "RUS": "2",
+      "EngShift": '"',
+      "RusShift": "@",  
+      "code": "Digit2"
+   },
+     {
+      "ENG": "3",
+      "RUS": "3",
+      "EngShift": "#",
+      "RusShift": "№",  
+      "code": "Digit3"
+   },
+     {
+      "ENG": "4",
+      "RUS": "4",
+      "EngShift": "$",
+      "RusShift": "%",  
+      "code": "Digit4"
+   },
+     {
+      "ENG": "5",
+      "RUS": "5",
+      "EngShift": "%",
+      "RusShift": ":",  
+      "code": "Digit5"
+   },
+     {
+      "ENG": "6",
+      "RUS": "6",
+      "EngShift": "^",
+      "RusShift": ":",  
+      "code": "Digit6"
+   },
+     {
+      "ENG": "7",
+      "RUS": "7",
+      "EngShift": "&",
+      "RusShift": "?",  
+      "code": "Digit7"
+   },
+     {
+      "ENG": "8",
+      "RUS": "8",
+      "EngShift": "*",
+      "RusShift": "*",  
+      "code": "Digit8"
+   },
+     {
+      "ENG": "9",
+      "RUS": "9",
+      "EngShift": "(",
+      "RusShift": "(",  
+      "code": "Digit9"
+   },
+     {
+      "ENG": "0",
+      "RUS": "0",
+      "EngShift": ")",
+      "RusShift": ")",  
+      "code": "Digit0"
+   },
+     {
+      "ENG": "-",
+      "RUS": "-",
+      "EngShift": "_",
+      "RusShift": "_",  
+      "code": "Minus"
+   },
+     {
+      "ENG": "=",
+      "RUS": "=",
+      "EngShift": "+",
+      "RusShift": "+",  
+      "code": "Equal"
+   },
+     {
+      "ENG": "Backspace",
+      "RUS": "Backspace",
+      "EngShift": "Backspace",
+      "RusShift": "Backspace",  
+      "code": "Backspace"
+   }],
+   [
+     {
+    "ENG": "Tab",
+    "RUS": "Tab",
+    "EngShift": "Tab",
+    "RusShift": "Tab",
+    "code": "Tab"
+   },
+     {
+      "ENG": "q",
+      "RUS": "й",
+      "EngShift": "Q",
+      "RusShift": "Й",  
+      "code": "KeyQ"
+   },
+     {
+      "ENG": "w",
+      "RUS": "ц",
+      "EngShift": "W",
+      "RusShift": "Ц",  
+      "code": "KeyW"
+   },
+     {
+      "ENG": "e",
+      "RUS": "у",
+      "EngShift": "E",
+      "RusShift": "У",  
+      "code": "KeyE"
+   },
+     {
+      "ENG": "r",
+      "RUS": "к",
+      "EngShift": "R",
+      "RusShift": "К",  
+      "code": "KeyR"
+   },
+     {
+      "ENG": "t",
+      "RUS": "е",
+      "EngShift": "T",
+      "RusShift": "Е",  
+      "code": "KeyT"
+   },
+     {
+      "ENG": "y",
+      "RUS": "н",
+      "EngShift": "Y",
+      "RusShift": "Н",  
+      "code": "KeyY"
+   },
+     {
+      "ENG": "u",
+      "RUS": "г",
+      "EngShift": "U",
+      "RusShift": "Г",  
+      "code": "KeyU"
+   },
+     {
+      "ENG": "i",
+      "RUS": "ш",
+      "EngShift": "I",
+      "RusShift": "Ш",  
+      "code": "KeyI"
+   },
+     {
+      "ENG": "o",
+      "RUS": "щ",
+      "EngShift": "O",
+      "RusShift": "Щ",  
+      "code": "KeyO"
+   },
+     {
+      "ENG": "p",
+      "RUS": "з",
+      "EngShift": "P",
+      "RusShift": "З",  
+      "code": "KeyP"
+   },
+     {
+      "ENG": "[",
+      "RUS": "х",
+      "EngShift": "{",
+      "RusShift": "Х",  
+      "code": "BracketLeft"
+   },
+     {
+      "ENG": "]",
+      "RUS": "ъ",
+      "EngShift": "}",
+      "RusShift": "Ъ",  
+      "code": "BracketRight"
+   },
+     {
+      "ENG": "\\",
+      "RUS": "\\",
+      "EngShift": "\\",
+      "RusShift": "\\",  
+      "code": "Backslash"
+   },
+     {
+      "ENG": "Del",
+      "RUS": "Del",
+      "EngShift": "Del",
+      "RusShift": "Del",  
+      "code": "Delete"
+   }],
+   [
+     {
+    "ENG": "CapsLk",
+    "RUS": "CapsLk",
+    "EngShift": "CapsLk",
+    "RusShift": "CapsLk",
+    "code": "CapsLock"
+   },
+     {
+      "ENG": "a",
+      "RUS": "ф",
+      "EngShift": "A",
+      "RusShift": "Ф",  
+      "code": "KeyA"
+   },
+     {
+      "ENG": "s",
+      "RUS": "ы",
+      "EngShift": "S",
+      "RusShift": "s",  
+      "code": "KeyS"
+   },
+     {
+      "ENG": "d",
+      "RUS": "в",
+      "EngShift": "D",
+      "RusShift": "в",  
+      "code": "KeyD"
+   },
+     {
+      "ENG": "f",
+      "RUS": "а",
+      "EngShift": "F",
+      "RusShift": "А",  
+      "code": "KeyF"
+   },
+     {
+      "ENG": "g",
+      "RUS": "п",
+      "EngShift": "G",
+      "RusShift": "П",  
+      "code": "KeyG"
+   },
+     {
+      "ENG": "h",
+      "RUS": "р",
+      "EngShift": "H",
+      "RusShift": "Р",  
+      "code": "KeyH"
+   },
+     {
+      "ENG": "j",
+      "RUS": "о",
+      "EngShift": "J",
+      "RusShift": "О",  
+      "code": "KeyJ"
+   },
+     {
+      "ENG": "k",
+      "RUS": "л",
+      "EngShift": "K",
+      "RusShift": "Л",  
+      "code": "KeyK"
+   },
+     {
+      "ENG": "l",
+      "RUS": "д",
+      "EngShift": "L",
+      "RusShift": "Д",  
+      "code": "KeyL"
+   },
+     {
+      "ENG": ";",
+      "RUS": "ж",
+      "EngShift": ":",
+      "RusShift": "Ж",  
+      "code": "Semicolon"
+   },
+     {
+      "ENG": "'",
+      "RUS": "э",
+      "EngShift": '"',
+      "RusShift": "Э",  
+      "code": "Quote"
+   },
+     {
+      "ENG": "Enter",
+      "RUS": "Enter",
+      "EngShift": "&#8629",
+      "RusShift": "&#8629",  
+      "code": "Enter"
+   }],
+   [
+     {
+    "ENG": "Shift",
+    "RUS": "Shift",
+    "EngShift": "&#8679",
+    "RusShift": "&#8679",
+    "code": "ShiftLeft"
+   },
+     {
+      "ENG": "z",
+      "RUS": "я",
+      "EngShift": "Z",
+      "RusShift": "Я",  
+      "code": "KeyZ"
+   },
+     {
+      "ENG": "x",
+      "RUS": "ч",
+      "EngShift": "X",
+      "RusShift": "Ч",  
+      "code": "KeyX"
+   },
+     {
+      "ENG": "c",
+      "RUS": "с",
+      "EngShift": "C",
+      "RusShift": "С",  
+      "code": "KeyC"
+   },
+     {
+      "ENG": "v",
+      "RUS": "м",
+      "EngShift": "V",
+      "RusShift": "М",  
+      "code": "KeyV"
+   },
+     {
+      "ENG": "b",
+      "RUS": "и",
+      "EngShift": "B",
+      "RusShift": "И",  
+      "code": "KeyB"
+   },
+     {
+      "ENG": "n",
+      "RUS": "т",
+      "EngShift": "N",
+      "RusShift": "Т",  
+      "code": "KeyN"
+   },
+     {
+      "ENG": "m",
+      "RUS": "ь",
+      "EngShift": "M",
+      "RusShift": "Ь",  
+      "code": "KeyM"
+   },
+     {
+      "ENG": ",",
+      "RUS": "б",
+      "EngShift": "<",
+      "RusShift": "Б",  
+      "code": "Comma"
+   },
+     {
+      "ENG": ".",
+      "RUS": "ю",
+      "EngShift": ">",
+      "RusShift": "Ю",  
+      "code": "Period"
+   },
+     {
+      "ENG": "/",
+      "RUS": ".",
+      "EngShift": "?",
+      "RusShift": ",",  
+      "code": "Slash"
+   },
+     {
+      "ENG": "&#8593",
+      "RUS": "&#8593",
+      "EngShift": "&#8593",
+      "RusShift": "&#8593",  
+      "code": "ArrowUp"
+   },
+     {
+      "ENG": "Shift",
+      "RUS": "Shift",
+      "EngShift": "&#8679",
+      "RusShift": "&#8679",  
+      "code": "ShiftRight"
+   }],
+   [
+     {
+    "ENG": "Ctrl",
+    "RUS": "Ctrl",
+    "EngShift": "Ctrl",
+    "RusShift": "Ctrl",
+    "code": "ControlLeft"
+   },
+     {
+      "ENG": "Win",
+      "RUS": "Win",
+      "EngShift": "Win",
+      "RusShift": "Win",  
+      "code": "MetaLeft"
+   },
+     {
+      "ENG": "Alt",
+      "RUS": "Alt",
+      "EngShift": "Win",
+      "RusShift": "Win",  
+      "code": "AltLeft"
+   },
+     {
+      "ENG": " ",
+      "RUS": " ",
+      "EngShift": " ",
+      "RusShift": " ",  
+      "code": "Space"
+   },
+     {
+      "ENG": "Alt",
+      "RUS": "Alt",
+      "EngShift": "Alt",
+      "RusShift": "Alt",  
+      "code": "AltRight"
+   },
+     {
+      "ENG": "&#8592",
+      "RUS": "&#8592",
+      "EngShift": "&#8592",
+      "RusShift": "&#8592",  
+      "code": "ArrowLeft"
+   },
+     {
+      "ENG": "&#8595",
+      "RUS": "&#8595",
+      "EngShift": "#8595",
+      "RusShift": "#8595",  
+      "code": "ArrowDown"
+   },
+     {
+      "ENG": "&#8594",
+      "RUS": "&#8594",
+      "EngShift": "&#8594",
+      "RusShift": "&#8594",  
+      "code": "ArrowRight"
+   },
+     {
+      "ENG": "Ctrl",
+      "RUS": "Ctrl",
+      "EngShift": "Ctrl",
+      "RusShift": "Ctrl",  
+      "code": "ControlRight"
+   }]
+  ]
+
+const BODY = document.querySelector('body');
+
+function createTitle () {    
+    const TITLE = document.createElement('div')
+    TITLE.className = 'title'
+    TITLE.innerHTML = 'RSS Виртуальная клавиатура'
+    BODY.append(TITLE)
+}
+function createTextArea () {    
+    const TEXTAREA = document.createElement('textarea')
+    TEXTAREA.className = 'text-area'
+    TEXTAREA.cols = '60'
+    TEXTAREA.rows = '8'
+    BODY.append(TEXTAREA)
+}
+function createKeyboard () {
+    const WRAPPER = document.createElement('div')
+    WRAPPER.className = 'wrapper'
+    BODY.append(WRAPPER)
+    for (i = 0; i < keysArr.length; i = i + 1) {
+        const LINE = document.createElement('div')
+        LINE.className = 'line'
+        WRAPPER.append(LINE)
+        for (l = 0; l < keysArr[i].length; l = l + 1) {
+            const KEY = document.createElement('div')
+            KEY.className = `key letter ${keysArr[i][l].code}`
+            KEY.innerHTML = keysArr[i][l].ENG
+            LINE.append(KEY)
+        }
+    }
+    const nonLetterKeys = document.querySelectorAll('.Backspace, .Tab, .Delete, .CapsLock, .Enter, .ShiftLeft, .ShiftRight, .ControlLeft, .MetaLeft, .AltLeft, .AltRight, .ControlRight')
+    nonLetterKeys.forEach((Element) => {Element.classList.remove('letter')})
+}
+function projectDescription () {
+    const DESCRIPTION = document.createElement('div')
+    DESCRIPTION.innerHTML = 'Клавиатура создана в операционной системе Windows. <br> Для переключения языка комбинация: Left ALT + Left Shift(Здравствуйте! Не успел закончить, перепроверьте пожалуйста в четверг вечером(оставьте дискорд для связи, напишу по готовности). Спасибо! Хорошего вам дня!)'
+    BODY.append(DESCRIPTION)
+}
+function createBody () {
+    createTitle()
+    createTextArea()
+    createKeyboard()
+    projectDescription()
+}
+createBody()
+
+const TEXTAREA = document.querySelector('.text-area')
+const LETTER = document.querySelectorAll('.letter')
+function enterLetters (event) {
+    TEXTAREA.value = TEXTAREA.value + event.target.innerHTML
+    TEXTAREA.focus()
+}
+LETTER.forEach((Element) => {Element.addEventListener('click', enterLetters)})
+
+function clickAnimationStart (event) {
+    event.target.classList.add('animation')
+}
+function clickAnimationEnd (event) {
+    event.target.classList.remove('animation')
+}
+const KEY = document.querySelectorAll('.key')
+KEY.forEach((Element) => {
+    Element.addEventListener('mousedown', clickAnimationStart)
+    Element.addEventListener('mouseout', clickAnimationEnd)
+    Element.addEventListener('mouseup', clickAnimationEnd)
+})
+function keyboardAnimationStart (event) {
+    TEXTAREA.focus()
+    const BUTTONPRESSED = document.querySelector(`.${event.code}`)
+    BUTTONPRESSED.classList.add('animation')
+}
+function keyboardAnimationEnd (event) {
+    const BUTTONPRESSED = document.querySelector(`.${event.code}`)
+    BUTTONPRESSED.classList.remove('animation')
+}
+
+document.addEventListener('keydown', keyboardAnimationStart)
+document.addEventListener('keyup', keyboardAnimationEnd)    
